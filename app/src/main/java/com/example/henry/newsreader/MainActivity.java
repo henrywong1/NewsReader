27db5c80adbe1ccb1af61e0b71824322599bf607
Henry Wong <H_wong14@u.pacific.edu>
2019-07-06 22:50:29 -0700

    Obtained info from api, obtained HTML info from url, started SQlite database

--- app/src/main/java/com/example/henry/newsreader/MainActivity.java
+++ app/src/main/java/com/example/henry/newsreader/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.henry.newsreader;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,10 +22,16 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> articles = new ArrayList<String>();
 
     ArrayAdapter arrayAdapter;
+
+    SQLiteDatabase articlesDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        articlesDatabase = this.openOrCreateDatabase("Articles", MODE_PRIVATE, null);
+        articlesDatabase.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, articleId INTEGER, title VARCHAR, content VARCHAR)");
+        
 
         ListView listView = findViewById(R.id.listView);
 
@@ -43,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... urls) {
-
-
             URL url;
             String result = "";
             HttpURLConnection urlConnection = null;
@@ -65,9 +73,56 @@ public class MainActivity extends AppCompatActivity {
                     result += curr;
 
                     data = inputStreamReader.read();
-
-
                 }
+                JSONArray jsonArray = new JSONArray(result);
+
+                int numOfArticles = 20;
+                if (jsonArray.length() < 20) {
+                    numOfArticles = jsonArray.length();
+                }
+                for (int i = 0; i < numOfArticles; i++) {
+                    String articleId = jsonArray.getString(i);
+                    url = new URL("https://hacker-news.firebaseio.com/v0/item/" + articleId + ".json?print=pretty");
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    inputStream = urlConnection.getInputStream();
+
+                    inputStreamReader = new InputStreamReader(inputStream);
+
+                    data = inputStreamReader.read();
+
+                    String articleInfo = "";
+                    while (data != -1) {
+                        char curr = (char) data;
+
+                        articleInfo += curr;
+
+                        data = inputStreamReader.read();
+                    }
+                    JSONObject jsonObject = new JSONObject(articleInfo);
+
+                    if (!jsonObject.isNull("title") && !jsonObject.isNull("url")) {
+                        String title = jsonObject.getString("title");
+                        String articleUrl = jsonObject.getString("url");
+
+                        url = new URL(articleUrl);
+
+                        urlConnection = (HttpURLConnection) url.openConnection();
+
+                        inputStream = urlConnection.getInputStream();
+                        inputStreamReader = new InputStreamReader(inputStream);
+                        data = inputStreamReader.read();
+                        String articleHtml = "";
+                        while (data != -1) {
+                            char curr = (char) data;
+                            articleHtml += curr;
+                            data = inputStreamReader.read();
+                        }
+                        Log.i("HTML" , articleHtml);
+
+                    }
+                }
+
                 Log.i("URL Content" , result);
 
                 return result;
@@ -77,5 +132,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+
     }
 }
